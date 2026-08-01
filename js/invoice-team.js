63be56e2d7588cc6d05984dca593a1b407565798
qsrjehann <hguy933@gmail.com>
2026-08-01 16:31:54 -0700

    Add files via upload

--- js/invoice-team.js
+++ js/invoice-team.js
@@ -20,8 +20,8 @@
    this sandbox blocks the Firebase CDN outright. See the Phase 6 report
    for exactly what could and could not be verified. */
 
-import { onAuthChange, getDb } from "./invoice-auth.js?v=20260802-0350";
-import { emailjsConfig, isEmailjsConfigured } from "./emailjs-config.js?v=20260802-0350";
+import { onAuthChange, getDb } from "./invoice-auth.js?v=20260802-0430";
+import { emailjsConfig, isEmailjsConfigured } from "./emailjs-config.js?v=20260802-0430";
 
 let currentUser = null;
 let members = [];
